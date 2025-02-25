47eff96cc0319a827ef6ecc8e96d54aeaa839760
dujmi <dujakmislav@gmail.com>
2025-02-25 23:49:41 +0100

    Finished implementation of dynamic routes for article page

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  output: "static",
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
