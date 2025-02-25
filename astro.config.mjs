6882b8812abd08c2649a4722b16b7eba1d9047f8
dujmi <dujakmislav@gmail.com>
2025-02-26 00:12:12 +0100

    Added sitemap, robots.txt and Astro's Image component instead of img

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,16 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
+  site: "https://www.my-site.dev",
   output: "static",
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [sitemap()],
 });
